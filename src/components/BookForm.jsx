051285f41f39c6490edf830ee9b01738ca334033
Kofi21 <owu.evander@gmail.com>
2025-05-25 18:40:28 -0400

    complete BookForm component

--- src/components/BookForm.jsx
+++ src/components/BookForm.jsx
@@ -6,8 +6,40 @@ const BookForm = ({setBooks}) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
+    if (!title.trim() || !author.trim()) 
+    return
+
+    const newBook = {
+      id: Date.now(),
+      title,
+      author,
+      read: false,
+    }
+
+    setBooks(prev => [...prev, newBook])
+    setTitle('')
+    setAuthor('')
   }
+
+  return (
+    <form onSubmit={handleSubmit} className="book-form">
+      <input 
+        type="text"
+        placeholder="Book Title"
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+        required
+      />
+    <input 
+      type="text"
+      placeholder="Author"
+      value={author}
+      onChange={e => setAuthor(e.target.value)}
+      required
+    />
+    <button type="submit">Add Book</button>
+    </form>
+  )
 }
 
 export default BookForm
